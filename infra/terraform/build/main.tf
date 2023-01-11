c32d212941113d32f58d74ccbe15d8bdca81621b
alex carvalho <alex.carvaho.data@gmail.com>
2023-01-12 00:21:58 +0100

    build: add terraform infrastructure (db)

---
+++ infra/terraform/build/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.25.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_volume" "postgres_django" {
+  name = "postgres-django"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:15"
+}
---
+++ infra/terraform/exec/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.25.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:15"
+  keep_locally = true
+}
+
+resource "docker_container" "postgres_django" {
+  name = "postgres_django"
+  image = docker_image.postgres.image_id
+  volumes {
+    container_path = "/var/lib/postgresql/data"
+    volume_name = "postgres-django"
+  }
+  env = [
+    "POSTGRES_DB=django_db",
+    "POSTGRES_USER=django",
+    "POSTGRES_PASSWORD=alex123x"
+  ]
+  ports {
+    internal = 5432
+    external = 5432
+  }
+}
