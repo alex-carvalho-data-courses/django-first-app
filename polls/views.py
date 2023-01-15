f6f33b40ce077699a42aed73d97e31326bb7f6f2
alex carvalho <alex.carvaho.data@gmail.com>
2023-01-15 18:25:54 +0100

    feat: index view latest 5 questions hard-coded

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from polls.models import Question
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request: HttpRequest) -> HttpResponse:
+    latest_5_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_5_questions])
+
+    return HttpResponse(output)
 
 
-def detail(request, question_id: int) -> HttpResponse:
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're looking at question {question_id}.")
 
 
-def results(request, question_id: int) -> HttpResponse:
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're looking at the results of question "
                         f"{question_id}.")
 
 
-def vote(request, question_id: int) -> HttpResponse:
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're voting on question {question_id}.")
