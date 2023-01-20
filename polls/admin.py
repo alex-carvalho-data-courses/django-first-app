44f4ff23ff15d38c333886905f5a64f94d1413a4
alex carvalho <alex.carvaho.data@gmail.com>
2023-01-20 22:28:16 +0100

    feat: add child Choice to Question Admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    # The following assignment determines the display order of the fields
-    # at the admin page
+    """The following assignment determines the display order of the fields
+    at the admin page. However, we'll organize the admin page by fieldsets.
     fields = ['pub_date', 'question_text']
+    """
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
