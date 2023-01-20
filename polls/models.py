b4b5e18f52b03e696b3b6b270713c33bde4c8cb9
alex carvalho <alex.carvaho.data@gmail.com>
2023-01-20 22:40:17 +0100

    feat: customize how the function was_published_recently is shown at the Question Admin List View

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,11 @@ class Question(models.Model):
     def __str__(self) -> str:
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?'
+    )
     def was_published_recently(self) -> bool:
         one_day_ago = timezone.now() - datetime.timedelta(days=1)
         return one_day_ago <= self.pub_date <= timezone.now()
