d44aeae93fe1eeb0dcb2fbfb08c0707cd036dd92
alex carvalho <alex.carvaho.data@gmail.com>
2023-01-20 20:18:11 +0100

    refactor: Add order to the Question Admin View fields

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Question
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # The following assignment determines the display order of the fields
+    # at the admin page
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
